8e962ed3c74d9da83e5d4160412f548f7ccc24ba
bazir123 <bazir123@users.noreply.github.com>
2026-07-24 07:33:15 -0400

    Update pyextracturl.py

--- pyextracturl.py
+++ pyextracturl.py
@@ -1,3 +1,9 @@
 import re
 INPUT_FILE = "input.txt"
 OUTPUT_FILE = "domains.txt"
+# Matches:
+# https://domain...
+# http://domain...
+# or plain domains
+pattern = re.compile(
+    r'((?:https?://)?(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})(?:[/?#:][^\s"\'>]*)?',
