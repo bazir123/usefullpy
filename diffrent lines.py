b00bda61e84918e4d1b385b7bb5ce3ca62c98b3b
bazir123 <bazir123@users.noreply.github.com>
2026-08-10 13:16:10 +0200

    Update diffrent lines.py

--- diffrent lines.py
+++ diffrent lines.py
@@ -3,3 +3,4 @@ with open("a.txt", "r", encoding="utf-8") as f:
 with open("b.txt", "r", encoding="utf-8") as f:
     b_lines = {line.strip().lower() for line in f}
 missing_lines = [
+    line for line in a_lines
