af0a6609b8c805b3221c26ff3ed10218bb32437a
bazir123 <bazir123@users.noreply.github.com>
2026-08-12 12:57:38 +0200

    Update diffrent lines.py

--- diffrent lines.py
+++ diffrent lines.py
@@ -5,3 +5,4 @@ with open("b.txt", "r", encoding="utf-8") as f:
 missing_lines = [
     line for line in a_lines
     if line.strip().lower() not in b_lines
+with open("c.txt", "w", encoding="utf-8") as f:
