fa1e1784a39f830ed23788d057ef80db958463e1
bazir123 <bazir123@users.noreply.github.com>
2026-08-21 13:05:51 +0200

    Update splitlines.py

--- splitlines.py
+++ splitlines.py
@@ -4,3 +4,4 @@ lines_per_file = 500
 with open(input_file, "r", encoding="utf-8") as f:
     lines = f.readlines()
 total_lines = len(lines)
+file_count = (total_lines + lines_per_file - 1) // lines_per_file
