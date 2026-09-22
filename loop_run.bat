05c7993e1b0fd0ab986f148339f28a0249361f00
bazir123 <bazir123@users.noreply.github.com>
2026-09-22 08:23:30 +0200

    Update loop_run.bat

--- loop_run.bat
+++ loop_run.bat
@@ -4,3 +4,4 @@ setlocal
 :: === CONFIGURATION ===
 set "PROGRAM_PATH="C:\Users\user\Desktop\program.exe""
 set "PROGRAM_NAME=program.exe"  :: Just the file name, used for killing
+set "DELAY_BEFORE_ENTER=3"
