be2628a6dd1e4149a99654500fc7b03766e2d222
bazir123 <bazir123@users.noreply.github.com>
2026-09-24 09:13:40 +0200

    Update loop_run.bat

--- loop_run.bat
+++ loop_run.bat
@@ -6,3 +6,4 @@ set "PROGRAM_PATH="C:\Users\user\Desktop\program.exe""
 set "PROGRAM_NAME=program.exe"  :: Just the file name, used for killing
 set "DELAY_BEFORE_ENTER=3"
 set "WAIT_TIME=100"                 :: Time in seconds (100 sec)
+set "DELAY_AFTER_CLOSE=2"
